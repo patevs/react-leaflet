2d835b5aa000f41bed8b220fd0362cc04094797e
patevs <ppevans11@gmail.com>
2018-08-12 18:12:14 +1200

    fixing dependencies in package.json

--- src/MainNav/MainNav.js
+++ src/MainNav/MainNav.js
@@ -9,45 +9,56 @@ class MainNav extends Component {
   render() {
     return (
       <div className="MainNav">
-      
-		<Navbar inverse collapseOnSelect>
-		  <Navbar.Header>
-		    <Navbar.Brand>
-		      <a href="#brand">Mate Freight | Logistics</a>
-		    </Navbar.Brand>
-		    <Navbar.Toggle />
-		  </Navbar.Header>
-		  <Navbar.Collapse>
-		    <Nav>
-		      <NavItem eventKey={1} href="#">
-		        Contact
-		      </NavItem>
-		      <NavItem eventKey={2} href="#">
-		        About
-		      </NavItem>
-		      <NavDropdown eventKey={3} title="Resources" id="basic-nav-dropdown">
-		        <MenuItem eventKey={3.1}>Action</MenuItem>
-		        <MenuItem eventKey={3.2}>Another action</MenuItem>
-		        <MenuItem eventKey={3.3}>Something else here</MenuItem>
-		        <MenuItem divider />
-		        <MenuItem eventKey={3.3}>Separated link</MenuItem>
-		      </NavDropdown>
-		    </Nav>
-		    <Nav pullRight>
-		      <NavItem eventKey={1} href="#">
-		      	  <ButtonToolbar>
-				    <Button><span className="glyphicon glyphicon-log-in"></span> Login</Button>
-				  </ButtonToolbar>
-		      </NavItem>
-		      <NavItem eventKey={2} href="#">
-		        <ButtonToolbar>
-				    <Button id="signup-btn" bsStyle="success"><span className="glyphicon glyphicon-user"></span> Sign Up</Button>
-				</ButtonToolbar>
-		      </NavItem>
-		    </Nav>
-		  </Navbar.Collapse>
-		</Navbar>
 
+        <div>
+		    <div>
+		      
+		      	<Navbar inverse collapseOnSelect>
+
+				  <Navbar.Header>
+				    <Navbar.Brand>
+				      <a href="#brand">Mate Freight | Logistics</a>
+				    </Navbar.Brand>
+				    <Navbar.Toggle />
+				  </Navbar.Header>
+
+				  <Navbar.Collapse>
+				  
+				    <Nav>
+				      <NavItem eventKey={1} href="#">
+				        Contact
+				      </NavItem>
+				      <NavItem eventKey={2} href="#">
+				        About
+				      </NavItem>
+				      <NavDropdown eventKey={3} title="Resources" id="basic-nav-dropdown">
+				        <MenuItem eventKey={3.1}>Action</MenuItem>
+				        <MenuItem eventKey={3.2}>Another action</MenuItem>
+				        <MenuItem eventKey={3.3}>Something else here</MenuItem>
+				        <MenuItem divider />
+				        <MenuItem eventKey={3.3}>Separated link</MenuItem>
+				      </NavDropdown>
+				    </Nav>
+
+				    <Nav pullRight>
+				      <NavItem eventKey={1} href="#">
+				      	  <ButtonToolbar>
+						    <Button><span className="glyphicon glyphicon-log-in"></span> Login</Button>
+						  </ButtonToolbar>
+				      </NavItem>
+				      <NavItem eventKey={2} href="#">
+				        <ButtonToolbar>
+						    <Button id="signup-btn" bsStyle="success"><span className="glyphicon glyphicon-user"></span> Sign Up</Button>
+						</ButtonToolbar>
+				      </NavItem>
+				    </Nav>
+
+				  </Navbar.Collapse>
+				</Navbar>
+
+		    </div>
+	    </div>
+      
       </div>
     );
   }
